cabbb5b8755a1f05b8f1f27447d36ae0f1cd3d31
OneTwo12 <jji042842@gmail.com>
2022-09-27 15:42:17 +0900

    Return User Information API

---
+++ src/test/java/dev/library/user/presentation/UserReturnTest.java
@@ -0,0 +1,25 @@
+package dev.library.user.presentation;
+
+import dev.library.dto.UserDTO;
+import dev.library.user.application.ReturnUserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@DisplayName("UserReturn 클래스")
+public class UserReturnTest {
+
+    @Autowired
+    ReturnUserService returnUserService;
+
+    @Test
+    public void userReturnTest(){
+        UserDTO dto = returnUserService.returnUserById("u-13-1");
+        System.out.println(dto);
+    }
+
+}
