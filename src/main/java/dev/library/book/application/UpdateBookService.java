6ee6c0bb72aabe2906059630af64403bcdbf5a1d
kyuri-L <julie7303@gmail.com>
2022-10-03 19:17:58 +0900

    add update book after rental

--- src/main/java/dev/library/book/application/UpdateBookService.java
+++ src/main/java/dev/library/book/application/UpdateBookService.java
@@ -1,2 +1,20 @@
-package dev.library.book.application;public class UpdateBookService {
+package dev.library.book.application;
+
+import dev.library.book.repository.BookRepository;
+import dev.library.user.domain.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UpdateBookService {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    public void update(Book updateState) {
+
+        Book updateBook = updateState.afterRentBookUpdateBook();
+
+        bookRepository.save(updateBook);
+    }
 }
--- src/main/java/dev/library/book/repository/BookRepository.java
+++ src/main/java/dev/library/book/repository/BookRepository.java
@@ -1,2 +1,9 @@
-package dev.library.book.repository;public interface BookRepository {
+package dev.library.book.repository;
+
+import dev.library.user.domain.Book;
+import dev.library.user.domain.BookId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, BookId> {
+
 }
