5dd9e568c0e19c0025b8d6dbe210ceea76e7c9e8
kyuri-L <julie7303@gmail.com>
2022-10-04 01:16:19 +0900

    merging conflict commit

--- src/main/java/dev/library/Rental/domain/RentalDate.java
+++ src/main/java/dev/library/Rental/domain/RentalDate.java
@@ -1,8 +1,8 @@
-package dev.library.Rental.domain;
+package dev.library.rental.domain;
 
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
+import net.bytebuddy.asm.Advice;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
@@ -11,7 +11,6 @@ import java.time.LocalDate;
 @Embeddable
 @ToString
 @Getter
-@Setter
 public class RentalDate {
 
     @Column(name = "rent_date")
@@ -19,5 +18,15 @@ public class RentalDate {
 
     @Column(name = "return_date")
     private LocalDate returnDate;
+
+    public RentalDate(LocalDate rentDate, LocalDate returnDate) {
+        this.rentDate = rentDate;
+        this.returnDate = this.rentDate.plusDays(7);
+    }
+
+    public Boolean checkReturnDeadline(){
+        LocalDate deadline = this.rentDate.plusDays(7);
+        return LocalDate.now().isAfter(deadline);
+    }
 
 }
